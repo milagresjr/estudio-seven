c52ecc5dbd6eb77fffd3bef44b81bc541dcfb7ae
Cristovao <raimundocristovaorc@gmail.com>
2026-02-09 15:07:21 +0100

    Close Modal apenas no botao

--- src/pages/admin/Projects.tsx
+++ src/pages/admin/Projects.tsx
@@ -278,7 +278,7 @@ const Projects = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center p-4"
-            onClick={closeModal}
+            
           >
             <motion.div
               initial={{ scale: 0.95, opacity: 0 }}
